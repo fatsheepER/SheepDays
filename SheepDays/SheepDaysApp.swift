07bc67efb69875d8aab8f015564a9fdf4c7ed236
fatsheepER <fatsheeper@icloud.com>
2026-04-01 16:43:52 +0800

    Initial Commit

---
+++ SheepDays/SheepDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheepDaysApp.swift
+//  SheepDays
+//
+//  Created by 王飞扬 on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct SheepDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
